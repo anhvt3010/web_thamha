b5520b283961a9c2c434b7986385d15a75ba4fae
Vu Tuan Anh <anh.vt.3010@gmail.com>
2023-11-28 11:49:40 +0700

    fix recommend input search product

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
 const products = [
     { name: 'Đèn Ốp Quạt Trần Ø1000mm MC-KD1364' },
     { name: 'Đèn Ốp Trần LED Hiện Đại L1090mm YN-1601-5' },
+    { name: 'Tuấn Anh' },
+    { name: 'aaAAAAA' },
     // Add more product data as needed
 ];
 
-const searchInput = document.getElementById('searchInput');
-const productDropdown = document.getElementById('productDropdown');
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('searchInput');
+    const productSuggestions = document.getElementById('productSuggestions');
+    const productList = document.getElementById('productList');
 
-searchInput.addEventListener('input', function () {
-    const inputValue = searchInput.value.toLowerCase();
-    const filteredProducts = products.filter(product => product.name.toLowerCase().includes(inputValue));
+    if (searchInput) {
+        // Thêm sự kiện input cho ô tìm kiếm
+        searchInput.addEventListener('input', function () {
+            const inputValue = searchInput.value.toLowerCase();
+            const filteredProducts = products.filter(product => product.name.toLowerCase().includes(inputValue));
 
-    // Clear existing options in the dropdown
-    productDropdown.innerHTML = '';
+            // Xóa tất cả các options hiện có trong datalist
+            productSuggestions.innerHTML = '';
 
-    if (filteredProducts.length > 0) {
-        // Populate the dropdown with matching products
-        filteredProducts.forEach(product => {
-            const option = document.createElement('option');
-            option.value = product.name;
-            productDropdown.appendChild(option);
+            if (filteredProducts.length > 0) {
+                // Thêm các options mới vào datalist
+                filteredProducts.forEach(product => {
+                    const suggestionOption = document.createElement('option');
+                    suggestionOption.value = product.name;
+                    productSuggestions.appendChild(suggestionOption);
+                });
+
+                // Hiển thị danh sách gợi ý
+                productList.innerHTML = ''; // Xóa nội dung hiện tại trong productList
+                productList.style.display = 'block';
+            } else {
+                // Ẩn danh sách gợi ý nếu không có sản phẩm nào phù hợp
+                productList.style.display = 'none';
+            }
         });
 
-        // Display the dropdown
-        productDropdown.style.display = 'block';
-    } else {
-        // Hide the dropdown if no matching products
-        productDropdown.style.display = 'none';
+        // Thêm sự kiện click cho ô tìm kiếm để tự động điền khi người dùng chọn đề xuất
+        searchInput.addEventListener('click', function () {
+            if (searchInput.value.trim() !== '') {
+                productList.style.display = 'none';
+            }
+        });
     }
 });
 
-document.getElementById('searchForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    // You can add additional handling here, such as navigating to a search results page
-});
+// document.getElementById('searchForm').addEventListener('submit', function (event) {
+//     event.preventDefault();
+//     // You can add additional handling here, such as navigating to a search results page
+// });
 
-// Handle selection from the dropdown
-productDropdown.addEventListener('change', function () {
-    searchInput.value = productDropdown.value;
-    productDropdown.style.display = 'none';
-});
+// // Handle selection from the dropdown
+// productDropdown.addEventListener('change', function () {
+//     searchInput.value = productDropdown.value;
+//     productDropdown.style.display = 'none';
+// });
